961f89d184e4ca802d9b87b2f5a65bd50f561c46
adithisatish <adithi.satish15@gmail.com>
2025-12-22 21:09:51 +0100

    archived individual agent folders and added coordinator agent code

--- backend/agents/coordinator/agent.py
+++ backend/agents/coordinator/agent.py
@@ -1,8 +1,15 @@
+from google.adk.agents import SequentialAgent
 from google.adk.agents.llm_agent import Agent
+from .task_agents import *
 
-root_agent = Agent(
-    model='<FILL_IN_MODEL>',
+cq_gen = get_cq_agent()
+rec_baseline = get_rec_baseline()
+
+coordinator = Agent(
+    model='gemini-2.5-flash',
     name='root_agent',
-    description='A helpful assistant for user questions.',
-    instruction='Answer user questions to the best of your knowledge',
+    description='A helpful coordinator for a travel recommender system which acts as an intermediary between the chat interface and the agentic backend.',
+    instruction='Given a user query, your task is to first provide the query to the clarifying question generator to obtain the clarifying questions. Also provide only the input query to the recommender agent. The final output MUST be a JSON combining the outputs of both the sub agents.',
 )
+
+root_agent = SequentialAgent(name="pipeline", sub_agents=[coordinator, cq_gen, rec_baseline])
